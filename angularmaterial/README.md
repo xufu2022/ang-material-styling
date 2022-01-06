4b4c8092b8c3274da63cbb47db23454d8eda1bdb
Your Name <you@example.com>
2022-01-06 16:01:25 +0000

    demo

--- angularmaterial/README.md
+++ angularmaterial/README.md
@@ -1,3 +1,8 @@
 # Angularmaterial
 
 ng add @angular/material
+ng --version
+
+https://material.angular.io/
+
+npm i -s @angular/flex-layout
--- angularmaterial/src/app/app.module.ts
+++ angularmaterial/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+const routes: Routes = [{ path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
